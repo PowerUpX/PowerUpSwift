5772edb8a329aa0172b04636c704542bab134e5d
Ceferino Jose II <cefjoeii@gmail.com>
2020-04-21 19:50:15 +0800

    Add asJSON computed property

--- PowerUpSwift/Classes/String.swift
+++ PowerUpSwift/Classes/String.swift
@@ -1,6 +1,6 @@
 //
 //  String.swift
-//  Pods
+//  PowerUpSwift
 //
 //  Created by Ceferino Jose II on 10/10/18.
 //
@@ -69,6 +69,43 @@ extension String {
     /// Reverses the value of `isValidMAC` so it feels more natural to write than using an exclamation point.
     public var isNotValidMAC: Bool {
         return !self.isValidMAC
+    }
+    
+    /// Returns the `Dictionary` form of the `String` if the format is valid or `nil` if invalid.
+    public var asJSON: [String: Any]? {
+        if let data = self.data(using: .utf8) {
+            do {
+                return try JSONSerialization.jsonObject(with: data, options: []) as? [String: Any]
+            } catch {
+                return nil
+            }
+        }
+        
+        return nil
+    }
+    
+    /// Parses and returns the payload `Dictionary` if the string is a JWT.
+    public var extractedJWTPayload: [String: Any]? {
+        // Seperate the strings by '.'
+        let segments = self.components(separatedBy: ".")
+        
+        // Get the middle part of the jwt which is a base 64 version of the payload
+        let base64Payload = segments[1]
+        
+        // I am not sure what's going on here but it works
+        let padded = base64Payload.padding(
+            toLength: ((base64Payload.count + 3) / 4) * 4,
+            withPad: "=",
+            startingAt: 0
+        )
+        
+        // Decode the base 64 string to `Dictionary`
+        if let decoded = Data(base64Encoded: padded),
+            let json = try? JSONSerialization.jsonObject(with: decoded, options: []) as? [String: Any] {
+            return json
+        }
+        
+        return nil
     }
     
     func limitLength(_ n: Int) -> String {

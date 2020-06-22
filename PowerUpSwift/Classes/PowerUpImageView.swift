c3fdfccf7ab4ebfdec35e3fe312de50caf3c9066
Ceferino Jose II <cefjoeii@gmail.com>
2020-06-22 12:07:01 +0800

    Fix the shadow color issue

--- PowerUpSwift/Classes/PowerUpImageView.swift
+++ PowerUpSwift/Classes/PowerUpImageView.swift
@@ -34,16 +34,17 @@ open class PowerUpImageView: UIImageView {
     }
     
     @IBInspectable open var shadowColor: UIColor? {
-        didSet { self.layer.shadowColor = self.borderColor?.cgColor }
+        didSet { self.layer.shadowColor = self.shadowColor?.cgColor }
     }
     
     open override func traitCollectionDidChange(_ previousTraitCollection: UITraitCollection?) {
         super.traitCollectionDidChange(previousTraitCollection)
         
+        // Handle the color update when switching to or from dark mode
         if #available(iOS 13.0, *) {
             if self.traitCollection.hasDifferentColorAppearance(comparedTo: previousTraitCollection) {
                 self.layer.borderColor = self.borderColor?.cgColor
-                self.layer.shadowColor = self.borderColor?.cgColor
+                self.layer.shadowColor = self.shadowColor?.cgColor
             }
         }
     }

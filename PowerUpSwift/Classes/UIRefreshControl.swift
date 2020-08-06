b215becd346a8ce37bc5926a4ce7ee903d60d737
Ceferino Jose II <cefjoeii@gmail.com>
2020-08-06 08:28:53 +0800

    Add  property on the UIRefreshControl extension

---
+++ PowerUpSwift/Classes/UIRefreshControl.swift
@@ -0,0 +1,15 @@
+//
+//  UIRefreshControl.swift
+//  PowerUpSwift
+//
+//  Created by Ceferino Jose II on 8/6/20.
+//
+
+import UIKit
+
+extension UIRefreshControl {
+    /// Reverses the value of `isRefreshing` so it feels more natural to write than using an exclamation point.
+    var isNotRefreshing: Bool {
+        return !self.isRefreshing
+    }
+}

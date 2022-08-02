d6dbe1e42ba6a84abb59e3bca588e8f64624f2af
Janzzen Matthew De Jesus <janzzen.dejesus@gmail.com>
2022-08-02 17:45:10 +0800

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -4,6 +4,7 @@ import PackageDescription
 
 let package = Package(
     name: "PowerUpSwift",
+    platforms: [.iOS(.v9), .tvOS(.v9)],
     products: [
         .library(name: "PowerUpSwift", targets: ["PowerUpSwift"])
     ],

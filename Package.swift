efd5a5f2d0283cdbdc47c064d0a1490df2213442
Janzzen Matthew De Jesus <janzzen.dejesus@gmail.com>
2022-08-02 17:25:20 +0800

    Create Package.swift

---
+++ Package.swift
@@ -0,0 +1,16 @@
+// swift-tools-version:5.0
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+import PackageDescription
+
+let package = Package(
+    name: "PowerUpSwift",
+    products: [
+        .library(name: "PowerUpSwift", targets: ["PowerUpSwift"])
+    ],
+    targets: [
+      .target(
+        name: "PowerUpSwift",
+        path: "PowerUpSwift"
+      )
+    ]
+)
